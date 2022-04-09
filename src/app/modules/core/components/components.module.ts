ffd5757acd29e431f8804f5a1f2136b1bcfd5824
kume1a <kumela011@gmail.com>
2022-04-09 13:23:45 +0400

    categories page ui

--- src/app/modules/core/components/components.module.ts
+++ src/app/modules/core/components/components.module.ts
@@ -3,10 +3,21 @@ import { StatCardComponent } from './stat-card/stat-card.component';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { CommonModule } from '@angular/common';
+import { MenuButtonComponent } from './menu-button/menu-button.component';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @NgModule({
-  imports: [NzCardModule, NzGridModule, CommonModule],
-  declarations: [StatCardComponent],
-  exports: [StatCardComponent],
+  imports: [
+    NzCardModule,
+    NzGridModule,
+    CommonModule,
+    NzButtonModule,
+    NzDropDownModule,
+    NzIconModule,
+  ],
+  declarations: [StatCardComponent, MenuButtonComponent],
+  exports: [StatCardComponent, MenuButtonComponent],
 })
 export class ComponentsModule {}
